5d6e84f93af688e86345efd21556767a5e1706fb
Debraj Goswami <59834809+Debraj8301@users.noreply.github.com>
2023-08-22 22:17:28 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-api.pulkitchauhan.me/' });
+const API = axios.create({ baseURL: 'https://throwback-server.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
